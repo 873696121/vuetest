b1509e99c2dd2265031c7a530f3be7cf129e64a5
胡宏 <huhong@192.168.0.104>
2021-01-25 19:50:58 +0800

    init

---
+++ src/views/PageOne.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+      <el-table-column
+          fixed
+          prop="id"
+          label="编号"
+          width="150">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="书名"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="author"
+          label="作者"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          fixed="right"
+          label="操作"
+          width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="total"
+        :page-size="pageSize"
+        @current-change="page">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    },
+    page(currentPage){
+      const _this = this
+      axios.get("http://localhost:8181/book/findAll/" + currentPage + "/6").then(function (resp){
+        _this.tableData = resp.data.content
+        _this.total = resp.data.totalElements
+        _this.pageSize = resp.data.size
+      })
+    }
+  },
+
+  created() {
+    const _this = this
+    axios.get('http://localhost:8181/book/findAll/1/6').then(function (resp){
+      _this.tableData = resp.data.content
+      _this.total = resp.data.totalElements
+      _this.pageSize = resp.data.size
+    })
+  },
+
+  data() {
+    return {
+      total: null,
+      tableData: null,
+      pageSize: null
+    }
+  }
+}
+</script>
